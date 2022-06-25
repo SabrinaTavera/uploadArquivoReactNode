db5541b7b8f0181f70c15cf46875172f20318894
SabrinaTavera <sabrina.tavera@hotmail.com>
2022-06-25 09:29:26 -0300

    Listagem de uploads

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,12 +2,30 @@ const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require('./config/multer');
 
-routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+const Post = require('./models/Post');
 
+routes.get('/posts', async(req, res) => {
+    console.log("entrei")
+    const posts = await Post.find();
+
+    return res.json(posts)
+})
+
+routes.post('/posts', multer(multerConfig).single('file'), async(req, res) => {
+
+    console.log(req.file)
+    const { originalname: name, size, filename: key, location: url = "" } = req.file;
+
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url,
+    })
     console.log(req.file);
 
 
-    return res.json({ message: "Hello Sasa" })
+    return res.json(post)
 })
 
 module.exports = routes;
